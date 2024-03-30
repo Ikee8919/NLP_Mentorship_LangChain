bb3f33b9a694b6fe4c6253bbd20e2e6faf058973
Ikee8919 <123157513+Ikee8919@users.noreply.github.com>
2024-03-30 17:02:43 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+PDF_FILE_PATH = r"D:\NLP_Mentorship\Notebooks\class3\documents\sensors-22-08281-v3.pdf"
+CSV_FILE_PATH = r'D:\NLP_Mentorship\Notebooks\class3\documents\test_csv.csv'
+PNG_FILE_PATH = r"D:\NLP_Mentorship\Notebooks\class3\documents\essay.png"
+JSON_FILE_PATH= r"D:\NLP_Mentorship\Notebooks\class3\documents\test_json.json"
+HTML_FILE_PATH= r"D:\NLP_Mentorship\Notebooks\class3\documents\test_html.html"
